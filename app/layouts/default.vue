a8c24e79fd95ef902a2f891a5c5b900328243edf
Herbert <hbtf.oliveira42@gmail.com>
2025-08-11 01:26:24 -0300

    Layout padrão

---
+++ app/layouts/default.vue
@@ -0,0 +1,43 @@
+<template>
+    
+    <v-app>
+        <v-container class="fill-height align-center align-content-center ga-4" max-width="90dvw" width="75dvw" >
+            
+            <header class="d-flex justify-center align-center w-100 bg-yellow-accent-2 rounded-xl px-5">
+                <v-spacer></v-spacer>
+                <h1>Header</h1>
+                <v-spacer></v-spacer>
+                <v-btn icon="mdi-close" text="Fechar" aria-label="Fechar" class="bg-red-accent-2 mr-5" />
+            </header>
+
+            <v-main class="h-75">
+                <v-card class="h-100" rounded="xl" >
+                    <slot />
+                </v-card>
+            </v-main>
+
+        </v-container>
+    </v-app>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+
+header {
+    height: 10dvh;
+}
+
+.v-application {
+    height: 100dvh;
+    background-image: url("~/assets/images/background.svg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: bottom;
+
+}
+
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-container fluid class="fill-height justify-center">
+        <h1>Main</h1>
+    </v-container>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>

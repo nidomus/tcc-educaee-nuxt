ce9d877ab233f0dd6d3e6bd97756e4d1ddd44f92
Herbert Oliveira <hbtf.oliveira42@gmail.com>
2025-08-19 13:25:07 -0300

    Pagina alunos pt1

--- app/pages/alunos/index.vue
+++ app/pages/alunos/index.vue
@@ -1,11 +1,61 @@
 <template>
 
+    <v-card class="h-100 bg-cyan-accent-2" rounded="xl" border="xl" density="comfortable">
+        <v-form class="h-100 py-5 px-5" @submit.prevent max-height="80%">
+            <v-container max-height="50%">
+                <v-row>
+                    <v-col cols="5">
+                        <v-text-field rounded label="Buscar aluno" color="cyan-darken-4" density="compact"
+                            variant="solo" append-inner-icon="mdi-magnify" hide-details></v-text-field>
+                    </v-col>
+                </v-row>
+
+                <v-row>
+                    <v-col cols="12">
+
+                        <v-table>
+                            <thead>
+                                <tr class="text-h5 font-weight-bold  ">
+                                    <th>Nome</th>
+                                    <th>Idade</th>
+                                    <th>Ano</th>
+                                    <th>Ações</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="a in alunos" :key="a.nome" class="text-subtitle-1">
+                                    <td>
+                                        <img :src="`/images/${a.foto}.jpg`" alt="Foto do aluno"></img>
+
+                                        <span>
+                                            {{ a.nome }}
+                                        </span>
+                                    </td>
+                                    <td>{{ a.idade }}</td>
+                                    <td>{{ a.ano }}</td>
+                                </tr>
+                            </tbody>
+                        </v-table>
+                    </v-col>
+                </v-row>
+
+            </v-container>
+
+        </v-form>
+
+    </v-card>
+
 </template>
 
 <script setup>
 
+import json from './alunos.json';
+
+
+const alunos = ref(json);
+
+
+
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,66 +1,64 @@
 <template>
-    <v-container class="h-100 " rounded="xl" border="xl" density="comfortable"  >
+    <v-container class="h-100 " rounded="xl" border="xl" density="comfortable">
         <v-row class="h-50">
-            <v-col cols="4"  height="100%" class="d-flex justify-center">
-                <v-btn stacked="" height="100%" width="85%" color="light-green-accent-3" class="d-flex justify-center ga-4">
+            <v-col cols="4" height="100%" class="d-flex justify-center">
+                <v-btn stacked="" height="100%" width="90%" color="light-green-accent-3"
+                    class="d-flex justify-center ga-4">
                     <span class="py-6 text-h4">
                         INICIAR SESSÃO
                     </span>
-                
-                    <v-img   width="10em"  src="~/assets/images/play.png"></v-img>
-               
+
+                    <v-img width="10em" src="~/assets/images/play.png"></v-img>
                 </v-btn>
             </v-col>
-            <v-col cols="4"  height="100%" class="d-flex justify-center">
-                <v-btn stacked height="100%" width="85%" color="cyan-accent-2" density="compact" >
+            <v-col cols="4" height="100%" class="d-flex justify-center">
+                <v-btn stacked height="100%" width="90%" color="cyan-accent-2" density="compact">
                     <span class="py-6 text-h4">
                         MEUS ALUNOS
                     </span>
-                    
-                    <v-img  width="20em"   src="~/assets/images/alunos.png"></v-img>
-               
+
+                    <v-img width="20em" src="~/assets/images/alunos.png"></v-img>
                 </v-btn>
             </v-col>
-            <v-col cols="4"  height="100%" class="d-flex justify-center">
-                <v-btn stacked="" height="100%" width="85%" color="purple-accent-2">
+            <v-col cols="4" height="100%" class="d-flex justify-center">
+                <v-btn stacked="" height="100%" width="90%" color="purple-accent-2">
                     <span class="py-6 text-h4">
                         ADICIONAR ALUNO
                     </span>
-                
-                    <v-img  width="10em"  src="~/assets/images/add_aluno.png"></v-img>
-               
+
+                    <v-img width="10em" src="~/assets/images/add_aluno.png"></v-img>
                 </v-btn>
             </v-col>
         </v-row>
-                <v-row class="h-50">
-            <v-col cols="4"  height="100%" class="d-flex justify-center">
-                <v-btn stacked="" height="100%" width="85%" color="orange-accent-3">
+        <v-row class="h-50">
+            <v-col cols="4" height="100%" class="d-flex justify-center">
+                <v-btn stacked="" height="100%" width="90%" color="orange-accent-3">
                     <span class="py-6 text-h4">
                         ATIVIDADES
                     </span>
-                
-                    <v-img  width="10em"  src="~/assets/images/atividades.png"></v-img>
-               
+
+                    <v-img width="10em" src="~/assets/images/atividades.png"></v-img>
+
                 </v-btn>
             </v-col>
-            <v-col cols="4"  height="100%" class="d-flex justify-center">
-                <v-btn stacked="" height="100%" width="85%" color="light-green-accent-3" >
+            <v-col cols="4" height="100%" class="d-flex justify-center">
+                <v-btn stacked="" height="100%" width="90%" color="light-green-accent-3">
                     <span class="py-6 pb-6 text-h4">
                         MINHAS SESSÕES
                     </span>
-                
-                    <v-img   width="10em"  src="~/assets/images/minhas_sessoes.png"></v-img>
-               
+
+                    <v-img width="15em" src="~/assets/images/minhas_sessoes.png"></v-img>
+
                 </v-btn>
             </v-col>
-            <v-col cols="4"  height="100%" class="d-flex justify-center">
-                <v-btn stacked=""  height="100%" width="85%" color="blue-grey-lighten-4">
+            <v-col cols="4" height="100%" class="d-flex justify-center">
+                <v-btn stacked="" height="100%" width="90%" color="blue-grey-lighten-4">
                     <span class="py-6 text-h4 ">
                         RELATÓRIOS
                     </span>
-                
-                    <v-img   width="10em"  src="~/assets/images/relatorio.png"></v-img>
-               
+
+                    <v-img width="15em" src="~/assets/images/relatorio.png"></v-img>
+
                 </v-btn>
             </v-col>
         </v-row>
@@ -73,17 +71,14 @@
 </script>
 
 <style lang="scss" scoped>
-
-.v-btn{
+.v-btn {
     border-radius: 15%;
     // box-shadow: -9px 9px 4px 3px rgba(0,0,0,0.59);
     // -webkit-box-shadow: -9px 9px 4px 3px rgba(0,0,0,0.59);
     // -moz-box-shadow: -9px 9px 4px 3px rgba(0,0,0,0.59);
 }
 
-.v-btn:focus{
+.v-btn:focus {
     outline: 4px solid rgb(24, 103, 192);
 }
-
-
 </style>
